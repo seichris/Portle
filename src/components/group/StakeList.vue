adf494820e5a02b14e94a8819fd14451d63f6a60
Timur Badretdinov <destinerx@gmail.com>
2020-02-09 12:57:01 +0300

    Add StakeList

---
+++ src/components/group/StakeList.vue
@@ -0,0 +1,108 @@
+<template>
+	<div>
+		<div id="list">
+			<StakeCard
+				v-for="stake in sortedStakes"
+				:key="stake.walletId + '-' + stake.assetId + '-' + stake.poolId"
+				:amount="stake.amount"
+				:pool-id="stake.poolId"
+				:wallet-id="stake.walletId"
+				:asset-id="stake.assetId"
+				:price="stake.price"
+			/>
+		</div>
+		<div id="table">
+			<StakeRow
+				v-for="stake in sortedStakes"
+				:key="stake.walletId + '-' + stake.assetId + '-' + stake.poolId"
+				:amount="stake.amount"
+				:asset-id="stake.assetId"
+				:pool-id="stake.poolId"
+				:wallet-id="stake.walletId"
+				:price="stake.price"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+import BigNumber from 'bignumber.js';
+
+import StakeCard from '../card/StakeCard.vue';
+import StakeRow from '../row/StakeRow.vue';
+
+import Converter from '../../utils/converter.js';
+
+export default {
+	components: {
+		StakeCard,
+		StakeRow,
+	},
+	props: {
+		stakes: {
+			type: Array,
+			default: () => [],
+		},
+		prices: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	computed: {
+		sortedStakes() {
+			const stakes = [];
+			for (const rawStake of this.stakes) {
+				const { walletId, assetId, poolId, balance } = rawStake;
+				const price = this.prices[assetId] || '0';
+				const amount = Converter.toAmount(balance, assetId);
+				const amountNumber = new BigNumber(amount);
+				const value = amountNumber.times(price).toString();
+				const asset = {
+					walletId,
+					assetId,
+					poolId,
+					amount,
+					price,
+					value,
+				};
+				stakes.push(asset);
+			}
+			stakes.sort((a, b) => {
+				const aValue = new BigNumber(a.value);
+				const bValue = new BigNumber(b.value);
+				return aValue.lt(bValue)
+					? 1
+					: aValue.gt(bValue)
+						? -1
+						: 0;
+			});
+			const meaningfulStakes = stakes.filter(asset => {
+				const value = new BigNumber(asset.value);
+				return value.gt(1);
+			});
+			return meaningfulStakes;
+		},
+	},
+};
+</script>
+
+<style scoped>
+#list {
+	display: flex;
+	flex-wrap: wrap;
+}
+
+#table {
+	display: none;
+}
+
+@media (max-width: 767px) {
+	#list {
+		display: none;
+	}
+
+	#table {
+		display: initial;
+	}
+}
+</style>
